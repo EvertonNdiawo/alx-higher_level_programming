973b40376fb4271d2721d78bed2bdfad0fbd92b6
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-09-16 23:38:10 +0300

    modify code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
+
 import add_0 as addition
-
-a = 1
-b = 2
-
 if __name__ == "__main__":
-    import sys
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, addition.add(a, b)))
