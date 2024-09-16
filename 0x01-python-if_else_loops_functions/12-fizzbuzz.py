9d1496803b585e291641f1c4c8d08e61d01da73c
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-09-16 11:49:47 +0300

    Correct pycodestyle for 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,11 +4,11 @@ def fizzbuzz():
     i = 1
     while i <= 100:
         if (i % 3 == 0 and i % 5 == 0):
-            print("{}".format('FizzBuzz'), end=" ")
-        elif(i % 3 == 0):
-            print("{}".format('Fizz'), end=" ")
-        elif(i % 5 == 0):
-            print("{}".format('Buzz'), end=" ")
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print('Fizz', end=" ")
+        elif i % 5 == 0:
+            print('Buzz', end=" ")
         else:
             print("{}".format(i), end=" ")
 
