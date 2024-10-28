4fee4eadecaaf2cd851e016bbd4bf8655cc10511
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-10-28 22:07:27 +0300

    Create file 4-new_in_list.py and 4-main.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+
+    else:
+        copy_of_list = my_list[:]
+        copy_of_list[idx] = element
+        return copy_of_list
+
