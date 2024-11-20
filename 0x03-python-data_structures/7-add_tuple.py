cbd83a18231c5cd39f4502373600c655888f03a5
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-11-20 19:32:16 +0300

    Refactor code to make it more simple and readable.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,35 +2,7 @@
 
 def add_tuple(tuple_a = (), tuple_b = ()):
 
-    
-    holder = 0
-    result = ()
-    
-    if (tuple_a and tuple_b):
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
 
-        if len(tuple_a) < 2:
-            result = ((tuple_a[0] + tuple_b[0]), (holder + tuple_b[1]))
-        
-    
-        elif len(tuple_b) < 2:
-            result = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + holder))
-         
-
-        elif (len(tuple_a) < 2) and (len(tuple_b) < 2):
-            result = ((tuple_a[0] + tuple_b[0]), (holder))
-          
-
-        else:
-            result = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
-    
-    elif not tuple_b:
-        result = ((tuple_a[0]), (tuple_a[1]))
-
-    elif not tuple_a:
-        result = ((tuple_b[0]), (tuple_b[1]))
-
-    else:
-        result = ((holder), (holder))
-    
-
-    return result
+    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
