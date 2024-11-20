a806bd01b6e274c28d192183a8a7a9ab90b4efd4
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-11-20 20:29:40 +0300

    Modify 9-max_integer.py to take care of a scenario where the list is empty.

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    max = my_list[0]
 
     if not my_list:
         return None
     else:
+        max = my_list[0]
+
         for x in my_list:
             if x >= max:
                 max = x
