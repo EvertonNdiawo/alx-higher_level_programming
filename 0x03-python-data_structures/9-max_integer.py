14fb51ac2c23dcaf1ba630f3e9ba70a8c3acb3cd
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-11-20 20:15:25 +0300

    Create files 9-main.py, a test file for 9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    max = 0
+
+    if not my_list:
+        return None
+    else:
+        for x in my_list:
+            if x >= max:
+                max = x
+
+    return max
