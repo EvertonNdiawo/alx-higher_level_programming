8184e23a3dc4fb13ef7cf09411bfc5d49b8063d0
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-11-20 19:55:59 +0300

    Create files 8-multiple_returns.py and 8-main.py, the test file

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    
+    first_char = ""
+    length_s = len(sentence)
+
+    if not sentence:
+        first_char = None
+
+    else:
+        first_char = sentence[0]
+
+    return (length_s, first_char)
