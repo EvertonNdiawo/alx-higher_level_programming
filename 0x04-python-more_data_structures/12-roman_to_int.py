37b1fbffcdb03f003ed0b51004b1ae2c20f7dee2
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-12-19 14:46:15 +0300

    Correct pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,12 @@
 
 def roman_to_int(roman_string):
     # Roman numeral single digits and substractive combinations
-    roman_dict = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
-    roman_sub = { 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900 }
-    
+    roman_dict = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+    }
+    roman_sub = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+
     # Handle invalid input
     if not roman_string or not isinstance(roman_string, str):
         return 0
@@ -17,14 +20,14 @@ def roman_to_int(roman_string):
         # Checks for two character match
         if i + 1 < len(roman_string) and roman_string[i:i+2] in roman_sub:
             numerals.append(roman_string[i:i+2])
-            i += 2 # Character found in a pair, so skip it and move to the next one.
+            i += 2  # Character found in a pair,skip it,move to next one.
         else:
             numerals.append(roman_string[i])
-            i += 1 # Move to next character
-    
+            i += 1  # Move to next character
+
     # Iterate through list and calculate total
     total = 0
-    
+
     for numeral in numerals:
         if numeral in roman_sub:
             total += roman_sub[numeral]
