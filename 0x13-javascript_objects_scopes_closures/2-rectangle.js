7445020a9f912efc02972af53956f43e22e45f4d
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-12-29 19:47:46 +0300

    Create

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+class Rectangle {
+  width;
+  height;
+
+  constructor (w, h) {
+    if (w > 0 && h > 0)
+    {
+      this.height = h;
+      this.width = w;
+    }
+  }
+}
+
+module.exports = Rectangle;
