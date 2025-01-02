8421a87cd1c1ab859cf1732ebe7986e26ed49cce
EvertonNdiawo <ndiawofreelancer@gmail.com>
2025-01-02 18:33:56 +0300

    Modify

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,16 +3,17 @@ const parentSquare = require('./5-square');
 
 class Square extends parentSquare {
   constructor (size) {
-    super(size);
-    this.size = size;
+    super(size, size);
+    this.height = size;
+    this.width = size;
   }
 
   charPrint (c) {
     if (!c) {
       c = 'X';
     }
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
